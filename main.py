f1b06160f98f5bdb037914e3b1794828d92439cf
Martín M <skydiver@users.noreply.github.com>
2021-05-12 00:57:16 -0300

    check if browser exists

--- main.py
+++ main.py
@@ -10,51 +10,59 @@ home = os.path.expanduser("~")
 
 chromium = [
   {
-    'name': 'CHROME',
-    'path': '/Library/Application Support/Google/Chrome',
-    'icon': 'chrome.icns'
+    "app" : "/Applications/Google Chrome.app",
+    "name": "CHROME",
+    "path": "/Library/Application Support/Google/Chrome",
+    "icon": "chrome.icns"
   },
   {
-    'name': 'CHROME_CANARY',
-    'path': '/Library/Application Support/Google/Chrome Canary',
-    'icon': 'canary.icns'
+    "app" : "/Applications/Google Chrome Canary.app",
+    "name": "CHROME_CANARY",
+    "path": "/Library/Application Support/Google/Chrome Canary",
+    "icon": "canary.icns"
   },
-  # {
-  #   'name': 'chromium',
-  #   'path': '/library/application support/chromium',
-  #   'icon': 'chromium.icns'
-  # },
   {
-    'name': 'BRAVE',
-    'path': '/Library/Application Support/BraveSoftware/Brave-Browser',
-    'icon': 'brave.icns'
+    "app" : "/Applications/Chromium.app",
+    "name": "chromium",
+    "path": "/Library/Application Support/Chromium",
+    "icon": "chromium.icns"
+  },
+  {
+    "app" : "/Applications/Brave Browser.app",
+    "name": "BRAVE",
+    "path": "/Library/Application Support/BraveSoftware/Brave-Browser",
+    "icon": "brave.icns"
   },
 ]
 
 firefox = [
   {
-    'name': 'FIREFOX',
-    'path': '/Library/Application Support/Firefox',
-    'icon': 'firefox.icns'
+    "app" : "/Applications/Firefox.app",
+    "name": "FIREFOX",
+    "path": "/Library/Application Support/Firefox",
+    "icon": "firefox.icns"
   },
   {
-    'name': 'FIREFOX_DEV',
-    'path': '/Library/Application Support/Firefox',
-    'icon': 'firefox-dev.icns'
+    "app" : "/Applications/FirefoxDeveloperEdition.app",
+    "name": "FIREFOX_DEV",
+    "path": "/Library/Application Support/Firefox",
+    "icon": "firefox-dev.icns"
   },
 ]
 
 profiles = []
 
 for browser in chromium:
-  path = "{}/{}".format(home, browser['path'])
-  prof = get_chromium_profiles(browser, path)
-  profiles += prof
+  if os.path.exists(browser['app']):
+    path = "{}/{}".format(home, browser['path'])
+    prof = get_chromium_profiles(browser, path)
+    profiles += prof
 
 for browser in firefox:
-  path = "{}/{}".format(home, browser['path'])
-  prof = get_firefox_profiles(browser, path)
-  profiles += prof
+  if os.path.exists(browser['app']):
+    path = "{}/{}".format(home, browser['path'])
+    prof = get_firefox_profiles(browser, path)
+    profiles += prof
 
 result = json.dumps({"items": profiles}, indent=2)
 print(result)
